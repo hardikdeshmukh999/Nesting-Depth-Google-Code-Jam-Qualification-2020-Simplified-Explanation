ebe997d0106fe893794c22871ce3a4314d33b438
Hardik Deshmukh <smarthardik10@gmail.com>
2020-04-22 18:03:44 +0530

    Add files via upload

---
+++ Nesting_Depth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Nesting Depth in Python
+
+# ##### Test 1 - 0000,(1)0(1), (111)000, (1)
+# ##### Test 2 - 0((2)1), (((3))1(2)), ((((4)))), ((2))0(((3))), ((2))((2))(1)
+
+# In[6]:
+
+
+m = input("Enter digits between - 0 to 9\n")
+m = [int(i) for i in m]
+n = []
+
+for i in m: 
+    
+    if (n.count('(')-n.count(')'))<=i:
+        while (n.count('(')-n.count(')')) != i:
+            n.append('(')            
+        n.append(i)        
+        
+    elif  (n.count('(')-n.count(')'))>i:
+        while (n.count('(')-n.count(')'))!=i:
+            n.append(')')
+        n.append(i)    
+        
+while (n.count('(')-n.count(')'))!=0:
+    n.append(')')
+    
+n = [str(i) for i in n]
+n="".join(n)
+print(n)
+
